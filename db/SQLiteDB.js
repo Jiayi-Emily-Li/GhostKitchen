d1fa33283c24f5b5422e6c9562c7533752f4d3d5
Katerina Bosko <katerinabosko@gmail.com>
2021-11-02 09:36:27 -0700

    working version for create a new order

--- db/SQLiteDB.js
+++ db/SQLiteDB.js
@@ -2,14 +2,106 @@ const sqlite3 = require("sqlite3");
 
 const { open } = require("sqlite");
 
-async function getMeals() {
-  const db = await open({
-    //needs 2 params
+async function connect() {
+  return open({
     filename: "./db/ghost_kitchen.db",
-    driver: sqlite3.Database,
+    //IMPORTANT: cached keyword to avoid opening db multople times
+    //otherwies SQL_BUSY error
+    //see https://github.com/mapbox/node-sqlite3/wiki/Caching
+    driver: sqlite3.cached.Database,
   });
-
-  return await db.all("SELECT * FROM Meal");
 }
 
-module.exports.getMeals = getMeals;
+async function getBrands() {
+  const db = await connect();
+  return await db.all("SELECT * FROM Virtual_Brand");
+}
+
+//to be used in index.ejs
+async function getMealsBy(brandID) {
+  const db = await connect();
+  const query = await db.prepare("SELECT * FROM Meal WHERE brand_id=:brandID");
+  query.bind({":brandID": brandID})
+  return await query.all()
+}
+
+//to be used in order.ejs
+async function getMeal(mealID) {
+  const db = await connect();
+  const query = await db.prepare("SELECT * FROM Meal WHERE id=:mealID");
+  query.bind({":mealID": mealID})
+  return await query.get()
+}
+
+async function getPickup() {
+  const db = await connect();
+  return await db.all("SELECT * FROM Pickup_Type");
+}
+
+async function getLocations() {
+  const db = await connect();
+  return await db.all("SELECT * FROM Location");
+}
+
+async function createOrder(order, userID) {
+  const db = await connect();
+  const order_time = new Date(Date.now())
+
+  const query = await db.prepare(`
+  INSERT INTO
+    Orders(order_time, quantity, location_id, customer_id, meal_id, pickup_id)
+    VALUES (:orderTime, :quantity, :location_id, :customer_id, :meal_id, :pickup_id);
+  `);
+  const vals = {
+    ":orderTime": order_time.toLocaleString(),
+    ":quantity": order.quantity,
+    ":location_id": order.location,
+    ":customer_id": userID,
+    ":meal_id": order.mealID,
+    ":pickup_id": order.pickup
+  }
+
+  console.log("will write these values for order:");
+  console.log(vals);
+
+  query.bind(vals);
+
+  return await query.run();
+}
+
+async function createPickupType(type) {
+  const db = await connect();
+
+  const query = await db.prepare(`
+  INSERT INTO
+    Pickup_Type(type)
+    VALUES (:type);
+  `);
+  const vals = {
+    ":type": type,
+  }
+
+  console.log(vals);
+  query.bind(vals);
+
+  return await query.run();
+}
+
+async function getUser(userId) {
+  const db = await connect();
+  const query = await db.prepare("SELECT * FROM Customer WHERE id=:userId");
+
+  query.bind({":userId": userId})
+  return await query.get()
+}
+
+module.exports = {
+  getMealsBy,
+  getUser,
+  getBrands,
+  getMeal,
+  getPickup,
+  getLocations,
+  createOrder,
+  createPickupType,
+};
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,69 @@
+const { render } = require("ejs");
 let express = require("express");
 let router = express.Router();
 
 const myDB = require("../db/SQLiteDB.js");
 
-/* GET home page. */
+/* Middleware for mock authentication */
+router.use("*", async function(req, res, next) {
+  // Mock authentication before every request
+  const user = await myDB.getUser(12);
+  req.user = user;
+  next()
+})
+
 router.get("/", async function (req, res, next) {
-  console.log("Got request for /");
-
-  const meals = await myDB.getMeals();
-
-  console.log("got meals", meals);
+  const brands = await myDB.getBrands();
+  console.log("got brands", brands);
   //render the _index_ template with the meals attribute as meals (from DB)
-  res.render("index", { meals: meals });
+  res.render("index", { brands: brands });
 });
 
-/* GET delete page. */
-router.get("/delete", function (req, res, next) {
-  //render the _index_ template with the title attribute as Express
-  res.render("index", { title: "DELETE" });
+/* GET menu page. */
+router.get("/brands/:brandID/menu", async function (req, res, next) {
+  //params come with GET, brandID is in params
+  console.log(`brandId is ${req.params.brandID}`);
+
+  const meals = await myDB.getMealsBy(req.params.brandID);
+
+  //render the _index_ template with the meals attribute as meals (from DB)
+  res.render("menu", { meals: meals });
+});
+
+/* GET order page. */
+router.get("/meals/:mealID/order", async function (req, res, next) {
+  console.log(`meal_id is ${req.params.mealID}`)
+  console.log(`user_id is ${req.user.id}`)
+
+  const meal = await myDB.getMeal(req.params.mealID);
+  const pickups = await myDB.getPickup();
+  const locations = await myDB.getLocations();
+
+  //render the order template with the meal attribute (created by getMeal)
+  res.render("order", { meal: meal, pickups: pickups, locations: locations});
+                      //^ var to be used in order.ejs
+});
+
+/* POST order page. */
+router.post("/order", async function (req, res, next) {
+  const userID = req.user.id;
+  const order = req.body;
+  console.log("got create order", order);
+
+  await myDB.createOrder(order, userID);
+  console.log(`Order created`);
+
+  res.redirect("/confirmation");
+
+});
+
+/* GET payment page. */
+router.get("/confirmation", async function (req, res, next) {
+
+  //const payment = await myDB.getOrder(req.params.brandID);
+
+  res.render("confirmation");
+
 });
 
 module.exports = router;
