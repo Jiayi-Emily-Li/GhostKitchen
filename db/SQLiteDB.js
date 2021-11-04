4e409c92aa73535cd0e5326e9a1524fdff4bd276
Emily Li <emily1998ljy@gmail.com>
2021-11-03 22:21:15 -0700

    Broke with sqlite when adding db close

--- db/SQLiteDB.js
+++ db/SQLiteDB.js
@@ -1,6 +1,7 @@
 const sqlite3 = require("sqlite3");
 
 const { open } = require("sqlite");
+const { query } = require("express");
 
 async function connect() {
   return open({
@@ -8,18 +9,26 @@ async function connect() {
     //IMPORTANT: cached keyword to avoid opening db multople times
     //otherwies SQL_BUSY error
     //see https://github.com/mapbox/node-sqlite3/wiki/Caching
-    driver: sqlite3.cached.Database,
+    driver: sqlite3.Database,
   });
 }
 
 async function getBrands() {
-  const db = await connect();
-  return await db.all("SELECT * FROM Virtual_Brand");
+  let db;
+  try{
+    db = await connect();
+    return await db.all("SELECT * FROM Virtual_Brand");
+  } finally {
+    db.close();
+  }
+  
 }
 
 async function getOrders(userID) {
-  const db = await connect();
-  const query = await db.prepare(
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare(
     `SELECT o.id, m.meal_name, m.description, m.price, o.quantity, pt.type
      FROM Orders AS o
      JOIN Meal AS m
@@ -28,15 +37,22 @@ async function getOrders(userID) {
      ON o.pickup_id = pt.id
      WHERE o.customer_id=:userID AND o.pickup_time IS NULL`);
 
-  query.bind({
-    ":userID": userID,
-  })
-  return await query.all();
+    query.bind({
+      ":userID": userID,
+    })
+    return await query.all();
+  }finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 async function getOrderByID(userID, orderID) {
-  const db = await connect();
-  const query = await db.prepare(
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare(
     `SELECT o.id AS order_id, o.quantity,
             m.id AS meal_id, m.meal_name, m.description,
             pt.id AS pickup_id, pt.type
@@ -47,169 +63,261 @@ async function getOrderByID(userID, orderID) {
      ON o.pickup_id = pt.id
      WHERE o.customer_id=:userID AND o.id=:orderID AND pickup_time IS NULL`);
 
-  query.bind({
-    ":userID": userID,
-    ":orderID": orderID,
-  });
-  console.log(`user id is ${userID}`);
-  console.log(`order id is ${orderID}`);
-  return await query.get();
+    query.bind({
+      ":userID": userID,
+      ":orderID": orderID,
+    });
+    console.log(`user id is ${userID}`);
+    console.log(`order id is ${orderID}`);
+    return await query.get();
+  }finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 async function updateOrder(orderID, quantity, pickupID, userID) {
-  const db = await connect();
-  const query = await db.prepare(
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare(
     `UPDATE Orders
      SET pickup_id=:pickupID,
          quantity=:quantity
      WHERE id=:orderID AND customer_id=:userID`);
 
-  query.bind({
-    ":pickupID": pickupID,
-    ":quantity": quantity,
-    ":orderID": orderID,
-    ":userID": userID,
-  });
-  console.log(`user id is ${userID}`);
-  console.log(`order id is ${orderID}`);
-  return await query.get();
+    query.bind({
+      ":pickupID": pickupID,
+      ":quantity": quantity,
+      ":orderID": orderID,
+      ":userID": userID,
+    });
+    console.log(`user id is ${userID}`);
+    console.log(`order id is ${orderID}`);
+    return await query.get();
+  } finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 async function deleteOrder(orderToDelete) {
-  const db = await connect();
+  let db, query;
+  try{
+    db = await connect();
 
-  const query = await db.prepare(
+    query = await db.prepare(
     `DELETE FROM
     Orders
     WHERE id = :theIDToDelete
     `);
 
-  query.bind({
-    ":theIDToDelete": orderToDelete,
-  });
-  return await query.run();
+    query.bind({
+      ":theIDToDelete": orderToDelete,
+    });
+    return await query.run();
+    }  finally {
+      await query.finalize();
+      await db.close();
+    }
+  
 }
 
 //to be used in index.ejs
 async function getMealsBy(brandID) {
-  const db = await connect();
-  const query = await db.prepare("SELECT * FROM Meal WHERE brand_id=:brandID");
-  query.bind({":brandID": brandID});
-  return await query.all();
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare("SELECT * FROM Meal WHERE brand_id=:brandID");
+    query.bind({":brandID": brandID});
+    return await query.all();
+  } finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 //to be used in order.ejs
 async function getMeal(mealID) {
-  const db = await connect();
-  const query = await db.prepare("SELECT * FROM Meal WHERE id=:mealID");
-  query.bind({":mealID": mealID});
-  return await query.get();
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare("SELECT * FROM Meal WHERE id=:mealID");
+    query.bind({":mealID": mealID});
+    return await query.get();
+  } finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 async function getPickup() {
-  const db = await connect();
-  return await db.all("SELECT * FROM Pickup_Type");
+  let db;
+  try{
+    db = await connect();
+    return await db.all("SELECT * FROM Pickup_Type");
+  } finally {
+    db.close();
+  }
+  
 }
 
 async function getLocations() {
-  const db = await connect();
-  return await db.all("SELECT * FROM Location");
+  let db;
+  try{
+    db = await connect();
+    return await db.all("SELECT * FROM Location");
+  } finally {
+    db.close();
+  }
+  
 }
 
 async function createOrder(order, userID) {
-  const db = await connect();
-  const order_time = new Date(Date.now());
+  let db, query;
+  try{
+    db = await connect();
+    const order_time = new Date(Date.now());
 
-  const query = await db.prepare(`
-  INSERT INTO
-    Orders(order_time, quantity, location_id, customer_id, meal_id, pickup_id)
-    VALUES (:orderTime, :quantity, :location_id, :customer_id, :meal_id, :pickup_id);
-  `);
-  const vals = {
-    ":orderTime": order_time.toLocaleString(),
-    ":quantity": order.quantity,
-    ":location_id": order.location,
-    ":customer_id": userID,
-    ":meal_id": order.mealID,
-    ":pickup_id": order.pickup
-  };
+    query = await db.prepare(`
+    INSERT INTO
+      Orders(order_time, quantity, location_id, customer_id, meal_id, pickup_id)
+      VALUES (:orderTime, :quantity, :location_id, :customer_id, :meal_id, :pickup_id);
+    `);
+    const vals = {
+      ":orderTime": order_time.toLocaleString(),
+      ":quantity": order.quantity,
+      ":location_id": order.location,
+      ":customer_id": userID,
+      ":meal_id": order.mealID,
+      ":pickup_id": order.pickup
+    };
 
-  console.log("will write these values for order:");
-  console.log(vals);
+    console.log("will write these values for order:");
+    console.log(vals);
 
-  query.bind(vals);
+    query.bind(vals);
 
-  return await query.run();
-                    //^^ run when a query doesn't return anything
+    return await query.run();
+                      //^^ run when a query doesn't return anything
+  } finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 async function getUser(userId) {
-  const db = await connect();
-  const query = await db.prepare("SELECT * FROM Customer WHERE id=:userId");
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare("SELECT * FROM Customer WHERE id=:userId");
 
-  query.bind({":userId": userId})
-  return await query.get()
+    query.bind({":userId": userId})
+    return await query.get();
+  } finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 /* ------Jiayi------ */
 async function createMeal(newMeal, brandID){
-  const db = await connect();
-  const query = await db.prepare(`INSERT INTO
-  Meal(meal_name, description, calories, price, brand_id)
-  VALUES (:meal_name, :description, :calories, :price, :brand_id)
-`);
-  query.bind({
-    ":meal_name": newMeal.meal_name,
-    ":description": newMeal.description,
-    ":calories": newMeal.calories,
-    ":price": newMeal.price,
-    ":brand_id": brandID,
-});
-  return await query.run();
+  //let db, query;
+  //try{
+    const db = await connect();
+    const query = await db.prepare(`INSERT INTO
+    Meal(meal_name, description, calories, price, brand_id)
+    VALUES (:meal_name, :description, :calories, :price, :brand_id)
+    `);
+    query.bind({
+      ":meal_name": newMeal.meal_name,
+      ":description": newMeal.description,
+      ":calories": newMeal.calories,
+      ":price": newMeal.price,
+      ":brand_id": brandID,
+    });
+    return await query.run();
+    // } finally {
+    //   //query.finalize();
+    //   db.close();
+    // }
+  
 }
 
 async function getBrandsBy(brandID) {
-  const db = await connect();
-  const query = await db.prepare("SELECT * FROM Virtual_Brand WHERE id=:brandID");
-  query.bind({":brandID": brandID});
-  return await query.get();
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare("SELECT * FROM Virtual_Brand WHERE id=:brandID");
+    query.bind({":brandID": brandID});
+    return await query.get();
+  } finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 async function updateMeal(mealID, brandID, meal_name, description, calories, price) {
-  const db = await connect();
-  const query = await db.prepare(
-    `UPDATE Meal
-     SET meal_name=:meal_name,
-         description=:description,
-         calories=:calories,
-         price=:price
-     WHERE id=:mealID AND brand_id=:brandID`);
+  let db, query;
+  try{
+    db = await connect();
+    query = await db.prepare(
+      `UPDATE Meal
+      SET meal_name=:meal_name,
+          description=:description,
+          calories=:calories,
+          price=:price
+      WHERE id=:mealID AND brand_id=:brandID`);
+      
+    const vals = {
+      "mealID": mealID,
+      "brandID": brandID,
+      "meal_name": meal_name,
+      "description": description,
+      "calories": calories,
+      "price": price
+    };
+    console.log("will update these values for meal:");
+    console.log(vals);
+    query.bind(vals);
 
-  query.bind({
-    "mealID": mealID,
-    "brandID": brandID,
-    "meal_name": meal_name,
-    "description": description,
-    "calories": calories,
-    "price": price
-  });
-  return await query.get();
+    return await query.get();
+  } finally {
+    await query.finalize();
+    await db.close();
+  }
+  
 }
 
 async function deleteMeal(mealToDelete) {
-  const db = await connect();
+  let db, query;
+  try{
+    db = await connect();
 
-  const query = await db.prepare(
-    `DELETE FROM
-    Meal
-    WHERE id = :theIDToDelete
-    `);
+    query = await db.prepare(
+      `DELETE FROM
+      Meal
+      WHERE id = :theIDToDelete
+      `);
 
-  query.bind({
-    ":theIDToDelete": mealToDelete,
-  });
-  return await query.run();
+    query.bind({
+      ":theIDToDelete": mealToDelete.id,
+    });
+    return await query.run();
+  } finally {
+    query.finalize();
+    await db.close();
+  }
+  
 }
+
 
 module.exports = {
   getMealsBy,
